9f69f3d0618e09717f822e8b5cf4c3c14090423a
Vladimir Yardan <vladfullstack@gmail.com>
2022-07-02 17:51:33 +0800

    about me changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -134,13 +134,13 @@ const IndexPage = () => (
   {/* <!-- ======================================= -->
   <!-- about me ======================================= -->
   <!-- ======================================= --> */}
-  <div class="lg:pt-12 pb-24 bg-gray-300 lg:text-center">
-    <div class="container mx-auto px-10">
+  <div className="lg:pt-12 pb-24 bg-gray-300 lg:text-center">
+    <div className="container mx-auto px-10">
 
-      <h2 class="mb-3 text-3xl lg:text-6xl font-bold text-teal-900">Some Tailwind Videos I've Done</h2>
+      <h2 className="mb-3 text-3xl lg:text-6xl font-bold text-teal-900">Some Tailwind Videos I've Done</h2>
 
-      <p class="mb-12 lg:text-xl leading-loose">Hey there! My name is <Link to="https://twitter.com/chris__sev"
-          class="text-blue-500 hover:underline">Chris Sev</Link> and I founded a web dev
+      <p className="mb-12 lg:text-xl leading-loose">Hey there! My name is <Link to="https://twitter.com/chris__sev"
+          className="text-blue-500 hover:underline">Chris Sev</Link> and I founded a web dev
         tutorial site Scotch.io. We were able to grow to 4M monthly
         pageviews and $300,000 yearly revenue. Scotch.io was acquired by DigitalOcean in November 2019. Nowadays I'm in
         search
@@ -148,20 +148,20 @@ const IndexPage = () => (
         MakeReactApps.com.</p>
 
       {/* <!-- videos --> */}
-      <div class="lg:flex lg:space-x-8 text-left space-y-8 lg:space-y-0">
-        <div class="lg:w-1/2 bg-white border-2 border-gray-700 rounded shadow-xl">
-          <p class="text-lg p-4 text-gray-700">Getting Started with Tailwind on scotch.io and a 15
+      <div className="lg:flex lg:space-x-8 text-left space-y-8 lg:space-y-0">
+        <div className="lg:w-1/2 bg-white border-2 border-gray-700 rounded shadow-xl">
+          <p className="text-lg p-4 text-gray-700">Getting Started with Tailwind on scotch.io and a 15
             minute starter YouTube video.</p>
 
-          <iframe class="w-full rounded-b" height="315" src="https://www.youtube.com/embed/6zIuAyLZPH0" frameborder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe title="Getting Started with Tailwind on scotch.io" className="w-full rounded-b" height="315" src="https://www.youtube.com/embed/6zIuAyLZPH0" frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
-        <div class="lg:w-1/2 bg-white border-2 border-gray-700 rounded shadow-xl">
-          <p class="text-lg p-4 text-gray-700">I did a Build Your 1st Tailwind Site for Traversy Media on YouTube also.
+        <div className="lg:w-1/2 bg-white border-2 border-gray-700 rounded shadow-xl">
+          <p className="text-lg p-4 text-gray-700">I did a Build Your 1st Tailwind Site for Traversy Media on YouTube also.
           </p>
 
-          <iframe class="w-full rounded-b" height="315" src="https://www.youtube.com/embed/6zIuAyLZPH0" frameborder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe title="Build Your 1st Tailwind Site" className="w-full rounded-b" height="315" src="https://www.youtube.com/embed/6zIuAyLZPH0" frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
       </div>
 
